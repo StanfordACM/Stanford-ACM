fc09d29139a29fcdf4b78c4e0ce3afa6900378bf
Scott Hickmann <scotthickmann21@gmail.com>
2022-09-21 13:51:50 -0700

    AI Reading Group -> Reading Group

--- pages/officers.tsx
+++ pages/officers.tsx
@@ -26,11 +26,11 @@ export default function Contact() {
           <Person name="Isaac Dan Zhao" year={2025} position="Vice-President" />
           <Person name="Sonny Young" year={2025} position="Puzzle Hunt Chair" />
           <Person name="Sydney Yan" year={2025} position="Puzzle Hunt Chair" />
-          <Person name="Ishan Khare" year={2025} position="AI Reading Group" />
+          <Person name="Ishan Khare" year={2025} position="Reading Group" />
           <Person
             name="Siddharth Sharma"
             year={2025}
-            position="AI Reading Group"
+            position="Reading Group"
           />
           <Person name="Rohan Sikand" year={2023} position="Social Chair" />
           <Person name="Rishi Verma" year={2025} position="ProCo Chair" />
