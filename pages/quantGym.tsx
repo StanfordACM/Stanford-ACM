1a66610d49b1cf762dae5cd4da79094e821a5467
ericcui <ericcui2005@gmail.com>
2024-11-11 16:16:18 -0800

    Minor Quant Gym modifications

--- pages/quantGym.tsx
+++ pages/quantGym.tsx
@@ -1,13 +1,14 @@
 import Layout from '../components/layout';
+import { QuantTitle } from '../components/quant';
 import { Paragraph, Section, Title } from '../components/section';
 
 export default function QuantGym() {
   return (
     <Layout title="Quant Gym" pageName="quantGym">
       <Section>
-        <Title>🏋️ Quant Gym 🏋️</Title>
+        <QuantTitle />
         <Paragraph>
-          Quant Gym is Stanford ACMs premier quantitative trading training
+          Quant Gym is Stanford ACM&apos;s premier quantitative trading training
           program. Get ready to flex those mind muscles, and let&apos;s work
           together to practice our skills and <b>ACE</b> the interviews 😤.
         </Paragraph>
