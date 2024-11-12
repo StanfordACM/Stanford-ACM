d8f15cbf71cbc35e5e13b5adeb30f3c3f0034b60
ericcui <ericcui2005@gmail.com>
2024-11-11 22:10:03 -0800

    Slight quant gym modifications

--- pages/quantGym.tsx
+++ pages/quantGym.tsx
@@ -1,6 +1,11 @@
 import Layout from '../components/layout';
 import { QuantTitle } from '../components/quant';
-import { Paragraph, Section, Title } from '../components/section';
+import {
+  Paragraph,
+  Section,
+  Title,
+  LinkComponent
+} from '../components/section';
 
 export default function QuantGym() {
   return (
@@ -9,12 +14,20 @@ export default function QuantGym() {
         <QuantTitle />
         <Paragraph>
           Quant Gym is Stanford ACM&apos;s premier quantitative trading training
-          program. Get ready to flex those mind muscles, and let&apos;s work
+          program. The key to getting a Quant Job is to practice Quant
+          Questions, so get ready to flex those mind muscles. Let&apos;s work
           together to practice our skills and <b>ACE</b> the interviews 😤.
         </Paragraph>
         <Paragraph>
           <b>Time: </b> Fridays 5 PM <br />
           <b>Location: </b> STLC 114 <br />
+        </Paragraph>
+        <Paragraph>
+          (If you are interested in partnering with us, please email{' '}
+          <LinkComponent href="mailto:stanfordquantgym@gmail.com">
+            stanfordquantgym@gmail.com
+          </LinkComponent>{' '}
+          to discuss details.)
         </Paragraph>
       </Section>
     </Layout>
